0d6412a417cdcdf2ecbcb807433411086ec65af5
Basile Masson <basile@Basiles-MacBook-Pro.local>
2020-06-17 15:41:55 +0200

    Revert "added files and changes required for deployment to heroku"
    This reverts commit e79905bdf1dfbabccca0083b9c2050680749c0f8.

--- ENTER/bin/pythonw
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-export PYTHONEXECUTABLE=/Users/basile/Desktop/Master Thesis/ENTER/bin/python
-/Users/basile/Desktop/Master Thesis/ENTER/python.app/Contents/MacOS/python "$@"
--- ENTER/shell/condabin/conda-hook.ps1
+++
@@ -1,8 +0,0 @@
-$Env:CONDA_EXE = "/Users/basile/Desktop/Master Thesis/ENTER/bin/conda"
-$Env:_CE_M = ""
-$Env:_CE_CONDA = ""
-$Env:_CONDA_ROOT = "/Users/basile/Desktop/Master Thesis/ENTER"
-$Env:_CONDA_EXE = "/Users/basile/Desktop/Master Thesis/ENTER/bin/conda"
-
-Import-Module "$Env:_CONDA_ROOT\shell\condabin\Conda.psm1"
-Add-CondaEnvironmentToPrompt
